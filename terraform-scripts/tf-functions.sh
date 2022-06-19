d9951ccc4fbe5508283c71152ab45a2ff20f0336
Mohammad Faraz <farazm708@gmail.com>
2022-06-18 23:48:45 -0400

    Added terraform get function

--- terraform-scripts/tf-functions.sh
+++ terraform-scripts/tf-functions.sh
@@ -3,6 +3,13 @@
 # This script holds bash functions to help with terraform.
 
 
+function get_terraform(){
+    TF_VERSION=$1
+    curl -fsSL -O https://releases.hashicorp.com/terraform/${TF_VERSION}/terraform_${TF_VERSION}_linux_amd64.zip
+    unzip terraform_${TF_VERSION}_linux_amd64.zip
+    chmod u+x ./terraform
+    echo "Terraform downloaded at `pwd`"
+}
 
 function terraform_set_path(){
     TERRAFORM_PATH=$1
